4303471fad15c10262933edb9e014a37db8df0a5
Shancheng Yang <shancheng.yang@ecovacs.com>
2020-11-03 09:20:07 +0800

    add the video link

--- ColleagueZone/ColleagueZone/View/EcoColleagueZoneVideoView.m
+++ ColleagueZone/ColleagueZone/View/EcoColleagueZoneVideoView.m
@@ -28,7 +28,7 @@
 
 - (void)setupSubViews{
     //1.播放网址
-    NSURL *mediaURL = [NSURL URLWithString:@"http://baobab.wdjcdn.com"];
+    NSURL *mediaURL = [NSURL URLWithString:@"http://dev-cc-static.ecovacs.cn/upload/colleague/video/weitie/2020/07/23/191104_5529.mp4.m3u8?pm3u8/0&e=1604450803&token=wTYFv7QhwqcpBOM31D3u_10aHEK2qk53hm5v4MZU:i3w8eeOUZGpgVnhlkb_iLuntamw="];
     self.item = [AVPlayerItem playerItemWithURL:mediaURL];
     
     //2.初始化一个播放器对象
@@ -36,7 +36,7 @@
     
     //3.初始化一个播放器layer
     self.playerLayer = [AVPlayerLayer playerLayerWithPlayer:self.myPlayer];
-    self.playerLayer.frame = self.bounds;
+    self.playerLayer.frame = CGRectMake(0,0,UIScreen.mainScreen.bounds.size.width,200);
     [self.layer addSublayer:self.playerLayer];
     
     //播放视频链接
